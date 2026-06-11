cfa9d6814ca7401bc4857ec2433e74ef65bded81
Gwangdana <17761997047@163.com>
2026-06-11 08:00:27 +0800

    handler架构

--- api-proxy-app/src/main/java/com/famsun/momapi/app/handler/BaseAppInfoHandler.java
+++
@@ -1,8 +0,0 @@
-package com.famsun.momapi.app.handler;
-
-import com.famsun.momapi.core.handler.BaseHandler;
-import org.springframework.stereotype.Component;
-
-@Component
-public class BaseAppInfoHandler extends BaseHandler {
-}
