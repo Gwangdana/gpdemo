15c8b4c0105747517d8fa83b541cab4997fde6ae
Gwangdana <17761997047@163.com>
2026-07-21 16:07:29 +0800

    CHANGE PROJECT 2 CLOUD

---
+++ auth/src/main/java/com/dana/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.dana.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 鉴权微服务启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
